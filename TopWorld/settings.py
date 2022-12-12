5b4dfd525b0b7e74d2eb9172c3aee7d1a95c26f8
Pramod Gopinath <gopinath.pramod@gmail.com>
2022-12-12 11:42:38 +0530

    static

--- TopWorld/settings.py
+++ TopWorld/settings.py
@@ -164,9 +164,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-    ]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+#     ]
 STATIC_ROOT = os.path.join(BASE_DIR,'newstatic')
 
 MEDIA_URL = '/media/'
