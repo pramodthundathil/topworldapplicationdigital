953fa6a528e7ac5668630ed81e58d30b8eba493f
Pramod Gopinath <gopinath.pramod@gmail.com>
2022-12-12 10:58:57 +0530

    delete heroku lib

--- TopWorld/settings.py
+++ TopWorld/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
-import django_heroku
+# import django_heroku
 
 import os
 import sys
@@ -176,4 +176,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
